522389350b95f27b912b667da65a7688f7aa511d
Ammisetty Yashwanth Bhargav <53438055+yashwanth-creater@users.noreply.github.com>
2021-12-18 02:32:31 +0530

    Create train_with_mask.py

---
+++ dataset/train_with_mask.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 10 13:34:20 2021
+
+@author: Bobby
+"""
+
+import cv2
+import os.path
+# Opens the inbuilt camera of laptop to capture video.
+cap = cv2.VideoCapture(0)
+i = 0
+path='and without dataset/with_mask'
+while(cap.isOpened()):
+    ret, frame = cap.read()
+     
+    # This condition prevents from infinite looping
+    # incase video ends.
+    if ret == False:
+        break
+     
+    # Save Frame by Frame into disk using imwrite method
+    cv2.imwrite(os.path.join(path,'Mask'+str(i)+'.jpg'), frame)
+    i += 1
+ 
+cap.release()
+cv2.destroyAllWindows()
